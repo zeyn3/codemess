80712496d53686e75d8958885de63f083d413543
khazarhaji <khazarhajiyev@gmail.com>
2019-11-11 16:41:08 +0400

    device_remover.py absolutely changed

--- projects/strep/strep_utils/device_remover.py
+++ projects/strep/strep_utils/device_remover.py
@@ -2,34 +2,26 @@ from datetime import datetime
 
 now = datetime.now()
 time = now.strftime("%D")
-recieved = input("Please enter the name of device to be removed: ")
+receive = input("Please enter the name of device to be removed: ")
+store = []
 
-def deactive(value):
-    
-    with open("../strep-master/devices.txt","r+") as devices:
-        if "T-01" in value:
-            devices.seek(62)
-            devices.write(" #deactivated on " + time)
-        elif "T-02" in value:
-            devices.seek(123)
-            devices.write(" #deactivated on " + time)
-        elif "T-03" in value:
-            devices.seek(184)
-            devices.write(" #deactiveted on " + time)
-        elif "T-04" in value:
-            devices.seek(245)
-            devices.write(" #deactivated on " + time)
-        elif "B-01" in value:
-            devices.seek(307)
-            devices.write(" #deactivated on " + time)
-        elif "B-02" in value:
-            devices.seek(368)
-            devices.write(" #deactivated on " + time)
-        elif "B-03" in value:
-            devices.seek(430)
-            devices.write(" #deactivated on " + time)
-        else:
-            print("There isn't device such {}".format(value))
+def check(arg):
+    if receive in arg[0]:
+        arg[1] = "deactivated on " + time + "\n"
+        arg = '#'.join([arg[0],arg[1]])
+        store.append(arg)
+    else:
+        arg[1] += "\n"
+        arg = "#".join([arg[0],arg[1]])
+        store.append(arg)
+        
 
+with open("../strep_master/devices.txt","r") as devices:
+    for device in devices:
+        if "\n" in device:
+            device = device[:-1]
+        seperate = device.split("#")
+        check(seperate)
 
-deactive(recieved)
+with open("../strep_master/devices.txt","w") as devices:
+    devices.writelines(store)
